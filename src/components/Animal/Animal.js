c17d7849b162b86d7e667723670a7167e90e2a96
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-09 14:43:49 -0800

    animals are rendered and positioned on page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,12 @@
-export default function Animal() {
-  return <></>;
+import React from 'react';
+import './Animal.css';
+
+export default function Animal({ name, says, top, left, type }) {
+  return (
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './Main.css';
+import Animal from '../Animal/Animal';
+import { animals } from '../../data.js';
 import background from '../../background.png';
 
 export default function Main() {
-  return <main className="main"></main>;
+  return (
+    <main className="main">
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }
