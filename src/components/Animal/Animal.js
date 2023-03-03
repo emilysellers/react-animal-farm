ffb0b3899a009a1444190fea31fc3ec7c3ffe54d
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-03 14:39:49 -0800

    remove unused code

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,7 +4,7 @@ import './Animal.css';
 export default function Animal({ name, says, top, left, type }) {
   return (
     <div className="animal" style={{ top, left }}>
-      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <img alt={name} src={`./animals/${type}.svg`} />
       <span className="name">{name}</span>
       <span>{says}</span>
     </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,14 +8,7 @@ export default function Main() {
   return (
     <main className="main">
       {animals.map((animal) => (
-        <Animal
-          key={animal.name}
-          name={animal.name}
-          type={animal.type}
-          top={animal.top}
-          left={animal.left}
-          says={animal.says}
-        />
+        <Animal key={animal.name} {...animal} />
       ))}
     </main>
   );
